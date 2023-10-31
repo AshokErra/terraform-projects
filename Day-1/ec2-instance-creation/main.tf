b82a541f402331b6a4111c4e6091b971f898eb9f
AshokErra <132039834+AshokErra@users.noreply.github.com>
2023-10-31 17:27:52 +0530

    Create main.tf

---
+++ Day-1/ec2-instance-creation/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0fc5d935ebf8bc3bc"  # Specify an appropriate AMI ID (Any OS Iamge from AWS)
+    instance_type = "t2.micro"
+}
